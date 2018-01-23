b4024570b045cf41cf9474bb6dd824b90e19018d
happy-thakur <suryanshsinghstudy@gmail.com>
2018-01-23 18:50:12 +0530

    FAB added

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,25 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the AboutPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { LeaderProvider } from '../../providers/leader/leader';
+import { Leader } from '../../shared/leader';
 
 @IonicPage()
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html',
 })
-export class AboutPage {
+export class AboutPage implements OnInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+        private leaderProvider: LeaderProvider,
+        @Inject('BaseURL') private BaseURL) {
   }
 
+  leaders: Leader[];
+  errMess: string;
   ionViewDidLoad() {
     console.log('ionViewDidLoad AboutPage');
+  }
+
+  ngOnInit() {
+    this.leaderProvider.getLeaders().subscribe((data) => {
+      this.leaders = data;
+    },
+    err => this.errMess = <any>err);
   }
 
 }
